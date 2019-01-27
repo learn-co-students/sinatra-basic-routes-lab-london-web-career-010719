2648db93ce7c5699c9b135d4883b9c47a4660dc1
Chris Camacho <cacamacho91@gmail.com>
2019-01-27 18:43:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Home"
+  end
+  get "/name" do
+    "My name is Chris"
+  end
+  get "/hometown" do
+    "My hometown is Strood"
+  end
+  get "/favorite-song" do
+    "My favorite song is Water by Anderson Paak"
+  end
 end
